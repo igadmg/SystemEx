0976b0402d51a50ddd32246da4c2d10cbebef1a6
Igor Karatayev <igor.cwer@gmail.com>
2014-05-20 17:01:34 +0300

    NET version update.

--- src/Tuple.cs
+++ src/Tuple.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace System
 {
-#if NET20
+#if NET35
 	public static class Tuple
 	{
 		public static Tuple<T1, T2> Create<T1, T2>(T1 item1, T2 item2) { return new Tuple<T1, T2>(item1, item2); }
