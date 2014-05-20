8e2515265044bfcde072a0c90b3a432d50fe7e86
Igor Karatayev <igor.cwer@gmail.com>
2014-05-20 16:39:45 +0300

    Extracted from UnityEngineEx.

---
+++ src/MarshalEx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SystemEx
+{
+	public static class MarshalEx
+	{
+		public static int SizeOf<T>() { return Marshal.SizeOf(typeof(T)); }
+
+		public static T PtrToStructure<T>(IntPtr data) { return (T) Marshal.PtrToStructure(data, typeof(T)); }
+
+
+		public static T BytesToStructure<T>(byte[] data)
+		{
+			GCHandle pdata = GCHandle.Alloc(data, GCHandleType.Pinned);
+			var r = MarshalEx.PtrToStructure<T>(pdata.AddrOfPinnedObject());
+			pdata.Free();
+			return r;
+		}
+
+		public static byte[] StructureToBytes<T>(T o)
+		{
+			byte[] data = new byte[MarshalEx.SizeOf<T>()];
+			GCHandle pdata = GCHandle.Alloc(data, GCHandleType.Pinned);
+			Marshal.StructureToPtr(o, pdata.AddrOfPinnedObject(), false);
+			pdata.Free();
+			return data;
+		}
+	}
+}
---
+++ src/UdpClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace SystemEx
+{
+	public static class UdpClientEx	
+	{
+		public static void Send(this UdpClient socket, byte[] data, IPEndPoint endpont)
+		{
+			socket.Send(data, data.Length, endpont);
+		}
+	}
+}
