124afea623887b21830ff2b9a8d7ca3c2b0504c7
Igor Karatayev <igor.cwer@gmail.com>
2014-06-12 20:06:44 +0300

    Func Containers

---
+++ src/FuncContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SystemEx
+{
+	public class FuncContainer
+	{
+		public Delegate action { get; protected set; }
+		public Type ret;
+		public Type[] args;
+
+		public FuncContainer(Delegate action, Type ret, params Type[] args)
+		{
+			this.action = action;
+			this.ret = ret;
+			this.args = args;
+		}
+
+		public object DynamicInvoke(params object[] args)
+		{
+			return action.DynamicInvoke(args);
+		}
+	}
+}
