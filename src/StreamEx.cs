f577314355093de9d7892bf93a7a80ec9a833364
Igor Karatayev <igor.cwer@gmail.com>
2014-06-14 03:55:54 +0300

    Stream CopyTo, String format.

---
+++ src/StreamEx.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace SystemEx
+{
+	public static class StreamEx
+	{
+		public static void CopyTo(this Stream input, Stream output)
+		{
+			byte[] buffer = new byte[32768];
+			int read;
+			while ((read = input.Read(buffer, 0, buffer.Length)) > 0) {
+				output.Write(buffer, 0, read);
+			}
+		}
+	}
+}
---
+++ src/StringEx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SystemEx
+{
+	public static class StringEx
+	{
+		public static string format(this string s, params object[] args)
+		{
+			return string.Format(s, args);
+		}
+	}
+}
