6f8781c01ce302c3decb2d69d949f5859d7d2af7
Igor Karatayev <igor.cwer@gmail.com>
2014-07-28 16:45:09 +0300

    AssemblyEx

---
+++ src/AssemblyEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace SystemEx
+{
+	public static class AssemblyEx
+	{
+		public static Assembly ExecutingAssembly { get { return Assembly.GetExecutingAssembly(); } }
+
+
+		public static IEnumerable<Type> EnumNamespace(this Assembly assembly, string namespaceName)
+		{
+			foreach (var type in assembly.GetTypes()) {
+				if (type.FullName.StartsWith(namespaceName)) {
+					yield return type;
+				}
+			}
+
+			yield break;
+		}
+	}
+}
