c7b45d0d6a2b08a8ed8950650edfcb3112f63c08
Igor Karatayev <igor.cwer@gmail.com>
2014-11-12 17:09:46 +0300

    Tuple sorting

---
+++ src/TupleEx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SystemEx
+{
+	public static class TupleEx
+	{
+		public static Tuple<T1, T1> Sort<T1>(this Tuple<T1, T1> t) where T1 : IComparable
+		{
+			if (t.Item1.CompareTo(t.Item2) > 0)
+				return Tuple.Create(t.Item2, t.Item1);
+			return t;
+		}
+	}
+}
