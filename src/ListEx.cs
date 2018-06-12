15e284e84914dc23e670cdd1926066c1e2f00b4e
Igor Karatayev <igor.cwer@gmail.com>
2018-06-12 10:29:03 +0300

    pop function.

--- src/ListEx.cs
+++ src/ListEx.cs
@@ -5,6 +5,13 @@ namespace SystemEx
 {
 	public static class ListEx
 	{
+		public static T Pop<T>(this List<T> list)
+		{
+			T t = list[list.Count - 1];
+			list.RemoveAt(list.Count - 1);
+			return t;
+		}
+
 		public static int Reserve<T>(this List<T> list, int size)
 		{
 			list.Capacity = Math.Max(list.Capacity, size);
