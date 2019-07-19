16b7fd3aeac00bece35672da7185ffcae0034cc7
Igor Karatayev <igor.cwer@gmail.com>
2019-07-19 18:56:41 +0300

    working on LineTokenizer

--- src/StringEx.cs
+++ src/StringEx.cs
@@ -7,13 +7,23 @@ namespace SystemEx
 {
 	public static class StringEx
 	{
-        /// <summary>
-        /// Foramat string as a member function of string.
-        /// </summary>
-        /// <param name="str"></param>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        public static string format(this string str, params object[] args)
+		public static bool null_(this string str)
+		{
+			return string.IsNullOrEmpty(str);
+		}
+
+		public static bool null_ws_(this string str)
+		{
+			return string.IsNullOrWhiteSpace(str);
+		}
+
+		/// <summary>
+		/// Foramat string as a member function of string.
+		/// </summary>
+		/// <param name="str"></param>
+		/// <param name="args"></param>
+		/// <returns></returns>
+		public static string format(this string str, params object[] args)
 		{
             return String.Format(str, args);
 		}
@@ -103,6 +113,150 @@ namespace SystemEx
 				index++;
 
 			return index;
+		}
+
+		public static int IndexOfReverse(this string str, char ch, int startIndex)
+		{
+			for (int i = startIndex; i >= 0; i--)
+			{
+				if (str[i] == ch)
+					return i;
+			}
+
+			return -1;
+		}
+
+		public static LineTokenizer tokenize(this string str)
+		{
+			return new LineTokenizer {
+				line = str
+			};
+		}
+
+		public static LineTokenizer tokenize(this string str, int si)
+		{
+			return new LineTokenizer
+			{
+				line = str,
+				li = si,
+				ei = si
+			};
+		}
+
+		public static bool find_any(this string str, out LineTokenizer tokenizer, out char ch, params char[] chars)
+		{
+			tokenizer = str.tokenize();			
+			return tokenizer.find_any(out ch, chars);
+		}
+
+		public static bool skip_whitespace(this string str, out LineTokenizer tokenizer, out char ch)
+		{
+			tokenizer = str.tokenize();
+			return tokenizer.skip_whitespace(out ch);
+		}
+
+		public static LineCutter cut(this string str)
+		{
+			return new LineCutter { result = str };
+		}
+	}
+
+	public class LineTokenizer
+	{
+		public int li = 0;
+		public int ei = 0;
+
+		public string line;
+
+		public bool end()
+		{
+			return ei == -1 || li == line.Length;
+		}
+
+		public string token()
+		{
+			return end() ? line.Substring(li) : line.Substring(li, ei - li);
+		}
+
+		public bool find_any(params char[] chars)
+		{
+			if (!skip())
+				return false;
+
+			ei = line.IndexOfAny(chars, li);
+
+			return !end();
+		}
+
+		public bool find_any(out char ch, params char[] chars)
+		{
+			ch = char.MaxValue;
+			if (find_any(chars))
+				ch = line[ei];
+
+			return !end();
+		}
+
+		public bool skip_whitespace()
+		{
+			if (!skip())
+				return false;
+
+			ei = li = line.SkipWhiteSpace(li);
+
+			return !end();
+		}
+
+		public bool skip_whitespace(out char ch)
+		{
+			ch = char.MaxValue;
+			if (skip_whitespace())
+				ch = line[ei];
+
+			return !end();
+		}
+
+		public bool skip()
+		{
+			li = ei + 1;
+
+			return !end();
+		}
+	}
+
+	public class LineCutter
+	{
+		public string result;
+		public int di = 0;
+
+		public LineCutter cut(int si, int ei)
+		{
+			si = si + di;
+			ei = ei + di;
+			di += 0 - (ei - si);
+
+			result = result
+					.Remove(si, ei - si);
+
+			return this;
+		}
+
+		public LineCutter replace(int si, int ei, string str)
+		{
+			si = si + di;
+			ei = ei + di;
+			di += str.Length - (ei - si);
+
+			result = result
+					.Remove(si, ei - si)
+					.Insert(si, str);
+
+			return this;
+		}
+
+		public static implicit operator string(LineCutter lc)
+		{
+			return lc.result;
 		}
 	}
 }
