c06994791e863941d2b18b7f2522f50a74b25ae4
Igor Karatayev <igor.cwer@gmail.com>
2020-03-29 11:25:15 +0300

    string hash function.

---
+++ src/HashEx.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SystemEx
+{
+	public static class HashEx
+	{
+		public static string HashSHA1(this string str, string format = "x2")
+		{
+			using (SHA1Managed sha1 = new SHA1Managed())
+			{
+				var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(str));
+				var sb = new StringBuilder(hash.Length * 2);
+
+				foreach (byte b in hash)
+				{
+					sb.Append(b.ToString(format));
+				}
+
+				return sb.ToString();
+			}
+		}
+	}
+}
