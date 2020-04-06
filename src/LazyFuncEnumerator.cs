56280cafcbf673352514009e9a67d11c8520019e
Igor Karatayev <igor.cwer@gmail.com>
2020-04-06 14:05:03 +0300

    Lazy stuff.

---
+++ src/LazyFuncEnumerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SystemEx
+{
+	public class LazyFuncEnumerator<T> : IEnumerator<T>
+	{
+		Func<T> func;
+		Lazy<T> current = new Lazy<T>();
+		public T Current => current.Value;
+		object IEnumerator.Current => Current;
+
+		public LazyFuncEnumerator(Func<T> fn_)
+		{
+			func = fn_;
+		}
+		public void Dispose() { }
+
+		public bool MoveNext()
+		{
+			Reset();
+			return true;
+		}
+
+		public void Reset()
+		{
+			if (current.IsValueCreated)
+				current = new Lazy<T>(() => func());
+		}
+	}
+}
---
+++ src/LazyNotNull.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SystemEx
+{
+	public class LazyNotNull<T>
+	{
+		Lazy<T> l;
+		Func<Lazy<T>> clfn;
+
+		public LazyNotNull() { clfn = () => l = new Lazy<T>(); l = clfn(); }
+		public LazyNotNull(bool isThreadSafe) { clfn = () => l = new Lazy<T>(isThreadSafe); }
+		public LazyNotNull(Func<T> valueFactory) { clfn = () => l = new Lazy<T>(valueFactory); l = clfn(); }
+		public LazyNotNull(LazyThreadSafetyMode mode) { clfn = () => l = new Lazy<T>(mode); l = clfn(); }
+		public LazyNotNull(Func<T> valueFactory, bool isThreadSafe) { clfn = () => l = new Lazy<T>(valueFactory, isThreadSafe); l = clfn(); }
+		public LazyNotNull(Func<T> valueFactory, LazyThreadSafetyMode mode) { clfn = () => l = new Lazy<T>(valueFactory, mode); l = clfn(); }
+
+		public bool IsValueCreated => l.IsValueCreated;
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		public T Value {
+			get {
+				T tv = l.Value;
+				if (tv == null)
+				{
+					l = clfn();
+				}
+				return tv;
+			}
+		}
+
+		public override string ToString() => l.ToString();
+	}
+}
