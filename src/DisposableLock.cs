a6cc0a03f4c4f11a523a5e92146fc145582fbfc3
Igor Karatayev <igor.cwer@gmail.com>
2020-04-07 00:04:14 +0300

    added DispoasableLock

---
+++ src/DisposableLock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SystemEx
+{
+	public static class DisposableLock
+	{
+		public static DisposableLock<T> Lock<T>(T v, Action<T> fn_)
+		{
+			return new DisposableLock<T>(v, fn_);
+		}
+	}
+
+	public class DisposableLock<T> : IDisposable
+	{
+		T v;
+		Action<T> disposeFn;
+
+		public DisposableLock(T v_, Action<T> disposeFn_)
+		{
+			v = v_;
+			disposeFn = disposeFn_;
+		}
+
+		public void Dispose()
+		{
+			disposeFn(v);
+		}
+
+		public T Value => v;
+		public static implicit operator T(DisposableLock<T> dl) => dl.v;
+	}
+}
