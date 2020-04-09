38ae6eecf2ff62bc31f7edeb586f65972d71d615
Igor Karatayev <igor.cwer@gmail.com>
2020-04-09 23:10:49 +0300

    untyped DisposableLock

--- src/DisposableLock.cs
+++ src/DisposableLock.cs
@@ -2,11 +2,28 @@
 
 namespace SystemEx
 {
-	public static class DisposableLock
+	public class DisposableLock : IDisposable
 	{
+		public static DisposableLock Lock(Action fn_)
+		{
+			return new DisposableLock(fn_);
+		}
+
 		public static DisposableLock<T> Lock<T>(T v, Action<T> fn_)
 		{
 			return new DisposableLock<T>(v, fn_);
+		}
+
+		Action disposeFn;
+
+		public DisposableLock(Action disposeFn_)
+		{
+			disposeFn = disposeFn_;
+		}
+
+		public void Dispose()
+		{
+			disposeFn();
 		}
 	}
 
