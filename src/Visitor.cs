9ff24d0650c404da6b21a5cb0821934dc3c3b5b1
Igor Karatayev <igor.cwer@gmail.com>
2021-02-22 23:19:51 +0300

    Added some Visitor stuff.

---
+++ src/Visitor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace SystemEx
+{
+	public interface IAccept<TVisitor>
+	{
+		void Accept(TVisitor visitor);
+	}
+
+	public interface IVisit<T>
+	{
+		void Visit(T item);
+	}
+
+	public static class VisitEx
+	{
+		/*
+		public static void Accept<T>(this IEnumerable<T> e, IVisit<T> visitor)
+			where T : IAccept<T>
+		{
+			foreach (var i in e)
+			{
+				i.Accept(visitor);
+			}
+		}
+		*/
+	}
+}
