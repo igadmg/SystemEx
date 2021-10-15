518f32a7ad52d716b5337eb60160d9098f851b76
Igor Karatayev <igor.cwer@gmail.com>
2021-10-15 15:32:12 +0300

    Added PathEx with Combine extension.

---
+++ src/PathEx.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace SystemEx
+{
+	public static class PathEx
+	{
+		public static string Combine(char separator, params string[] paths)
+			=> Path.Combine(paths).Replace(Path.DirectorySeparatorChar, separator);
+	}
+}
