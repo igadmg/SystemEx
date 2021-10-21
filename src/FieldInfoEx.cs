5a1788def36179f930833b6a908ad503d075a40c
Igor Karatayev <igor.cwer@gmail.com>
2021-10-21 17:15:20 +0300

    Update.

---
+++ src/FieldInfoEx.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace SystemEx
+{
+	public static class FieldInfoEx
+	{
+		public static T GetValue<T>(this FieldInfo fi, object o)
+			=> (T)fi.GetValue(o);
+	}
+}
