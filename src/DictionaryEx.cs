604908e32760b572d8a346c6a1d9700d67cec8c7
Igor Karatayev <igor.cwer@gmail.com>
2022-03-07 13:10:02 +0300

    added decompose for net framework KeyValuePair

--- src/DictionaryEx.cs
+++ src/DictionaryEx.cs
@@ -156,5 +156,13 @@ namespace SystemEx
 
 			d.Clear();
 		}
+
+#if NET20_OR_GREATER
+		public static void Deconstruct<T1, T2>(this KeyValuePair<T1, T2> tuple, out T1 key, out T2 value)
+		{
+			key = tuple.Key;
+			value = tuple.Value;
+		}
+#endif
 	}
 }
