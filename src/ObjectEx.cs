23c129d84b859162fdc3ee5208a2c649b6e396d1
Igor Karatayev <igor.cwer@gmail.com>
2022-05-18 19:04:28 +0300

    Disposable.

--- src/ObjectEx.cs
+++ src/ObjectEx.cs
@@ -112,50 +112,5 @@ namespace SystemEx
 				return hash;
 			}
 		}
-
-		public static void DisposeFields(this object o)
-		{
-			foreach (var field in o.GetType().GetFields<DisposeAttribute>())
-			{
-				if (field.FieldType.IsA<IDictionary<object, IDisposable>>())
-				{
-					var dict = field.GetValue(o) as IEnumerable;
-					if (dict == null) continue;
-
-					foreach (object p in dict)
-					{
-						p.GetFieldValue<IDisposable>("Value")?.Dispose();
-					}
-				}
-				else if (field.FieldType.IsA<IList<IDisposable>>())
-				{
-					var list = field.GetValue(o) as IEnumerable;
-					if (list == null) continue;
-
-					foreach (object p in list)
-					{
-						(p as IDisposable)?.Dispose();
-					}
-				}
-				else if (field.FieldType.IsA<Lazy<IDisposable>>())
-				{
-					var lazy = field.GetValue(o);
-					if (lazy.GetPropertyValue<bool>("IsValueCreated"))
-					{
-						lazy.GetFieldValue<IDisposable>("Value")?.Dispose();
-					}
-				}
-				else if (field.FieldType.IsA<IDisposable>())
-				{
-					(field.GetValue(o) as IDisposable).Elvis(d => d.Dispose());
-				}
-				else
-				{
-#if UNITY
-					UnityEngine.Debug.LogWarning($"Don't know how to dispose field: {field.FieldType.Name} {o.GetType().Name}.{field.Name}");
-#endif
-				}
-			}
-		}
 	}
 }
