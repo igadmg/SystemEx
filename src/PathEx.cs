4ddfaa7254a2a6edfb664bc8cfbfd33e2dd08c40
Igor Karatayev <igor.cwer@gmail.com>
2022-06-18 00:12:46 +0300

    add aslinuxpath

--- src/PathEx.cs
+++ src/PathEx.cs
@@ -8,7 +8,7 @@ namespace SystemEx
 		public static string AsPath(this ValueTuple<string, string> paths)
 			=> Path.Combine(paths.Item1, paths.Item2);
 
-		public static string Combine(char separator, params string[] paths)
-			=> Path.Combine(paths).Replace(Path.DirectorySeparatorChar, separator);
+		public static string AsLinuxPath(this string path)
+			=> path.Replace(Path.DirectorySeparatorChar, '/');
 	}
 }
