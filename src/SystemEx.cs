3dd093728b1b5f524d45be493d16ae69d7fdfe32
Igor Karatayev <igor.cwer@gmail.com>
2022-12-01 03:43:40 +0300

    update

---
+++ src/SystemEx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SystemEx
+{
+	public static class SystemEx
+	{
+		public static Lazy<T> lazy<T>(Func<T> fn) => new Lazy<T>(() => fn());
+
+		public static DisposableLock dlock(Action fn) => DisposableLock.Lock(fn);
+		public static DisposableLock<T> dlock<T>(T v, Action<T> fn) => DisposableLock.Lock(v, fn);
+	}
+}
